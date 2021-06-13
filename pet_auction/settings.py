e53be91c4e60bdf42847a7eebb6a0bd1c5509fee
Crazy Cookie <mr.tmk_0210@mail.ru>
2021-06-13 19:09:10 +0600

    wrapped in docker-compose

--- pet_auction/settings.py
+++ pet_auction/settings.py
@@ -148,5 +148,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Celery Configuration Options
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
